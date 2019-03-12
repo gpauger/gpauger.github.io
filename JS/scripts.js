a89242e4e80697eefcf50507e7693aa619aee3e3
Greg Auger <gpauger@gmail.com>
2019-03-11 20:38:25 -0700

    test recommendation comments
    Minimal changes after testing out a new section

--- JS/scripts.js
+++ JS/scripts.js
@@ -4,4 +4,6 @@ window.onscroll = () => {
     const nav = document.querySelector('#navbar');
     if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
   };
+  
   /* End navbar scroll effect*/
+  
